0690eeb04a7b283c7a9cec5eeec4fdd2c2ae645d
Shreshth Juneja <shreshthj5@gmail.com>
2026-08-10 23:57:28 +0530

    Time: 0 ms (100%), Space: 11.9 MB (18.34%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> answer;
+        for (int i=0;i<n;i++){
+            if ((i+1)%3==0 && (i+1)%5==0){
+                answer.push_back("FizzBuzz");
+            }
+            else if ((i+1)%3==0){
+                answer.push_back("Fizz");
+            }  
+            else if ((i+1)%5==0){
+                answer.push_back("Buzz");
+            } 
+            else{
+                answer.push_back(to_string(i+1));
+            }
+        }
+        return answer;
+    }
+};
